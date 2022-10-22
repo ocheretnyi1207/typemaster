a9f05bcc1c19754b05bec4e2a0d09b6bb7366bee
ocheretnyi <ocheretnyi.spb@gmail.com>
2022-10-22 22:38:02 +0300

    sync

--- gulpfile.js
+++ gulpfile.js
@@ -14,21 +14,22 @@ gulp.task("css", () => {
       .pipe(gulp.dest("build/css"))
       .pipe(browsersync.stream())
   });
-  
+
   gulp.task("copy", () => {
     return gulp.src([
         "#src/img/**/*.{jpeg,jpg,png,svg}",
+        "#src/fonts/**/*.{woff2,woff}",
         "#src/index.html"
       ], {
         base: "#src"
       })
       .pipe(gulp.dest("build"))
   });
-  
+
   gulp.task("clean", () => {
     return del("build")
   });
-  
+
   gulp.task("server", () => {
     browsersync.init({
       server: "build/",
@@ -37,16 +38,16 @@ gulp.task("css", () => {
       cors: true,
       ui: false
     })
-  
+
     gulp.watch("#src/sass/**/*.sass", gulp.series("css", "refresh"));
     gulp.watch("#src/index.html", gulp.series("copy"));
     gulp.watch("#src/index.html").on("change", browsersync.reload);
   });
-  
+
   gulp.task("refresh", (done) => {
     browsersync.reload();
     done();
   });
-  
+
   gulp.task("build", gulp.series("clean", "css", "copy"));
   gulp.task("start", gulp.series("build", "server"));
